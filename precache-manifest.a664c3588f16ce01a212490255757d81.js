4eecd56c7b3ed3489edff6d9258c35d3bf81e319
Aizat Faiz <aizat.faiz@gmail.com>
2020-02-26 21:26:42 +0700

    Updates

---
+++ precache-manifest.a664c3588f16ce01a212490255757d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df66b03ed951d33d39ffdfcf538c4641",
+    "url": "/giphy-markdown/index.html"
+  },
+  {
+    "revision": "c304d591d5b11c6e9878",
+    "url": "/giphy-markdown/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "ea85964ca839639ec87b",
+    "url": "/giphy-markdown/static/css/main.cdce6f6e.chunk.css"
+  },
+  {
+    "revision": "c304d591d5b11c6e9878",
+    "url": "/giphy-markdown/static/js/2.552b4d93.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/giphy-markdown/static/js/2.552b4d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea85964ca839639ec87b",
+    "url": "/giphy-markdown/static/js/main.b754255e.chunk.js"
+  },
+  {
+    "revision": "34fb663413625fd9c2e2",
+    "url": "/giphy-markdown/static/js/runtime-main.6a817ab5.js"
+  }
+]);
